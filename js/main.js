92f95c1c0911f2c27788556327d9a6aeb04b0ae7
Paula Faria <paula.faria@planb.com.br>
2013-08-23 13:35:46 -0300

    define cores

--- js/main.js
+++ js/main.js
@@ -5,18 +5,16 @@ $(function(){
 		var total = 0;
 
 		$('.skills-input').each(function(){
-			var inputs = $(this).children('div').children('input:checked');
-			var selects = $(this).children('div').children('select');
+			var inputs = $(this).children('div').children('label').children('input:checked');
+			var selects = $(this).children('div').children('label').children('select');
+			
 			if(inputs.size() > 0){
-
 				var totalInput = inputs.val();
 				var totalSelect = selects.val();
 				total += totalInput*totalSelect;
-
-				console.log(total);
-
 			}
 		});
+
 
 		if(total <= 810){
 			var cargo = 'junior'
@@ -25,18 +23,8 @@ $(function(){
 		}else{
 			var cargo = 'senior'
 		}
-		console.log(cargo);
+
 		$('#total').html(total + ' - ' + cargo);
 		e.preventDefault();
 	});
-
-	$(document).on('click','.skills-input', function(){
-		var inputs = $(this).children('div').children('input');
-
-		if(inputs.attr('checked')){
-			inputs.attr('checked', false);
-		}else{
-			inputs.attr('checked', true);
-		}
-	});
-})
+});

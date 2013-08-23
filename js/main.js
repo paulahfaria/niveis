7de6dda5566e08d80d1f6d27129e2aac43c49818
Paula Faria <paulah37@gmail.com>
2013-08-23 00:08:04 -0300

    lista todas as skills e define niveis

--- js/main.js
+++ js/main.js
@@ -5,18 +5,38 @@ $(function(){
 		var total = 0;
 
 		$('.skills-input').each(function(){
-			var inputs = $(this).children('input:checked');
-			var selects = $(this).children('input:checked');
+			var inputs = $(this).children('div').children('input:checked');
+			var selects = $(this).children('div').children('select');
 			if(inputs.size() > 0){
 
 				var totalInput = inputs.val();
 				var totalSelect = selects.val();
 				total += totalInput*totalSelect;
+
 				console.log(total);
 
 			}
 		});
-		$('#total').html(total);
+
+		if(total <= 810){
+			var cargo = 'junior'
+		} else if (total > 810 && total <= 1620) {
+			var cargo = 'pleno'
+		}else{
+			var cargo = 'senior'
+		}
+		console.log(cargo);
+		$('#total').html(total + ' - ' + cargo);
 		e.preventDefault();
+	});
+
+	$(document).on('click','.skills-input', function(){
+		var inputs = $(this).children('div').children('input');
+
+		if(inputs.attr('checked')){
+			inputs.attr('checked', false);
+		}else{
+			inputs.attr('checked', true);
+		}
 	});
 })
